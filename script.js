9f1086929bda378b25edf273f30bda0cf2a6bf6b
Zuhriddin <nuriddinovsirojoddin@gmail.com>
2020-12-01 15:23:49 +0500

    jsstart

---
+++ script.js
@@ -0,0 +1,27 @@
+var javob = prompt('Ismingzni kiriting ')
+console.log('Sizning ismingiz: ' + javob);
+
+var javob = prompt('Yoshingizni kiriting: ')
+console.log('Sizning yoshinigiz: ' + javob);
+
+var javob = prompt('Misolni yeching:30 + 15 ')
+console.log('Misol 1: 30+15=' + (30+15));
+console.log('Sizning javob: ' + javob);
+
+var javob = prompt('Misolni yeching:25-15 ')
+console.log('Misol 2: 25-15=' + (25-15));
+console.log('Sizning javob: ' + javob);
+
+var javob = prompt('Misolni yeching:15*15 ')
+console.log('Misol 3: 15*15=' + (15*15));
+console.log('Sizning javob: ' + javob);
+
+var javob = prompt('Misolni yeching:30/15 ')
+console.log('Misol 4: 30/15=' + (30/15));
+console.log('Sizning javob: ' + javob);
+
+var javob = prompt('Misolni yeching:30%16 ')
+console.log('Misol 5: 30%15=' + (30%15));
+console.log('Sizning javob: ' + javob);
+
+alert('Javoblarni korish uchun consolga qarang ')
